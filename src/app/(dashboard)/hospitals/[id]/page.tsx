a2420729d9598d425597041850e5a2bbd49dd2c7
Reaper1x0 <musa_b_k@hotmail.com>
2026-02-09 23:40:17 +0500

    initial commit

--- src/app/(dashboard)/hospitals/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function LegacyHospitalDetailPage({ params }: { params: { id: string } }) {
-  redirect(`/dashboard/hospitals/${params.id}`);
-}
--- src/app/(dashboard)/hospitals/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function LegacyHospitalsPage() {
-  redirect("/dashboard/hospitals");
-}
